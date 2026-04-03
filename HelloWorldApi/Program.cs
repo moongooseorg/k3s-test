6bf4fc2b0ad26c2cb7ae66bc6bd4f8710891bcea
Daniel Moon <daniel_moon16@yahoo.com>
2026-04-03 19:33:36 -0400

    testing out pipeline

--- HelloWorldApi/Program.cs
+++ HelloWorldApi/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/hello", () => new { message = "Hello, World!", timestamp = DateTime.UtcNow });
+app.MapGet("/hello", () => new { message = "Hello, World 2!", timestamp = DateTime.UtcNow });
 
 app.Run();
